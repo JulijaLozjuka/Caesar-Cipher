579050ed41c8ea50bb9b2bfc718e95a70edde718
JuliaJinxty <jinxty@inbox.lv>
2022-03-19 03:02:19 +0200

    Finished brute force decrypt method

--- src/java/com/javarush/cryptoanalyser/CaesarEncoder.java
+++ src/java/com/javarush/cryptoanalyser/CaesarEncoder.java
@@ -1,58 +1,117 @@
 package com.javarush.cryptoanalyser;
 
 import java.io.*;
+import java.util.*;
 
 public class CaesarEncoder {
 
+    private String getEncryptedStringFromArray(int key, char[] charArray) {
+        StringBuilder result = new StringBuilder();
+        for (char c : charArray) {
+            boolean isUpperCase = Character.isUpperCase(c);
+            String s = (c + "").toLowerCase();
+            if (CaesarData.ALPHABET.contains(s)) {
+                int pos = CaesarData.ALPHABET.indexOf(s);
+                int newPos = (pos + key) % CaesarData.ALPHABET.length();
+                char encryptedChar = newPos >= 0
+                        ? CaesarData.ALPHABET.charAt(newPos)
+                        : CaesarData.ALPHABET.charAt(CaesarData.ALPHABET.length() + newPos);
+
+                s = String.valueOf(encryptedChar);
+            }
+            if (isUpperCase) {
+                s = s.toUpperCase();
+            }
+            result.append(s);
+        }
+        return result.toString();
+    }
+
+    private String getDecryptedStringFromArray(int key, char[] charArray) {
+        return getEncryptedStringFromArray(-key, charArray);
+    }
 
     public void encryptWithKey(int key, String sourceFileName, String destFileName) {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(sourceFileName));
              BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(destFileName))) {
 
             while (bufferedReader.ready()) {
-                StringBuilder result = new StringBuilder();
                 char[] charArray = new char[1024];
-                int readBytes = bufferedReader.read(charArray);
-                for (int i = 0; i <readBytes ; i++) {
-                    boolean isUpperCase = Character.isUpperCase(charArray[i]);
-                    String s = (charArray[i] + "").toLowerCase();
-                    if (CaesarData.ALPHABET.contains(s)) {
-                        int pos = CaesarData.ALPHABET.indexOf(s);
-                        int newPos = (pos + key) % CaesarData.ALPHABET.length();
-                        char encryptedChar = newPos>=0
-                                ? CaesarData.ALPHABET.charAt(newPos)
-                                :CaesarData.ALPHABET.charAt(CaesarData.ALPHABET.length()+newPos);
-
-                            s = String.valueOf(encryptedChar);
-                    }
-                    if(isUpperCase){
-                        s=s.toUpperCase();
-                    }
-                    result.append(s);
+                int readCharsCount = bufferedReader.read(charArray);
+                if (charArray.length > readCharsCount) {
+                    charArray = Arrays.copyOf(charArray, readCharsCount);
                 }
-                bufferedWriter.write(result.toString());
+
+                String resultString = getEncryptedStringFromArray(key, charArray);
+
+                bufferedWriter.write(resultString);
             }
 
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-    }
-    public void decryptWithKey(int key, String sourceFileName, String destFileName){
-        encryptWithKey(-key,sourceFileName,destFileName);
     }
 
-    public void decryptBruteForce(String sourceFileName, String destFileName){
-        for (int i = 1; i <CaesarData.ALPHABET.length() ; i++) {
-            decryptWithKey(i,sourceFileName,destFileName);
+    public void decryptWithKey(int key, String sourceFileName, String destFileName) {
+        encryptWithKey(-key, sourceFileName, destFileName);
+    }
 
+    public void decryptBruteForce(String sourceFileName, String destFileName) {
+        List<String> readLines = new ArrayList<>();
+        Map<Integer, Integer> matchingWordCountByKey = new HashMap<>();
+
+
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(sourceFileName))) {
+            int currentStringCount = 0;
+            while (bufferedReader.ready() && currentStringCount < 50) {
+                readLines.add(bufferedReader.readLine());
+                currentStringCount++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 1; i < CaesarData.ALPHABET.length(); i++) {
+            List<String> decryptedWords = new ArrayList<>();
+            int matchingWordCount = 0;
+
+            for (String readLine : readLines) {
+                char[] charArray = readLine.toCharArray();
+                String decryptedString = getDecryptedStringFromArray(i, charArray);
+                String[] splitDecryptedStrings = decryptedString.split(" ");
+                for (String s : splitDecryptedStrings) {
+                    if (!s.isBlank()) {
+                        decryptedWords.add(s.trim());
+                    }
+                }
+
+            }
+
+            for (String decryptedWord : decryptedWords) {
+                if (CaesarData.topWords.contains(decryptedWord)) {
+                    matchingWordCount++;
+                }
+            }
+            matchingWordCountByKey.put(i,matchingWordCount);
 
         }
 
+        int bestKey = 0;
+        int maxWordCount = 0;
+        for (Map.Entry<Integer,Integer> pairs: matchingWordCountByKey.entrySet()){
+            if (pairs.getValue()>maxWordCount){
+                maxWordCount= pairs.getValue();
+                bestKey = pairs.getKey();
+            }
+        }
+
+        decryptWithKey(bestKey,sourceFileName,destFileName);
+
     }
 
-    public void decryptStatisticMethod(String sourceFileName, String destFileName, String exampleFileName){
+    public void decryptStatisticMethod(String sourceFileName, String destFileName, String exampleFileName) {
 
     }
 
--- src/java/com/javarush/cryptoanalyser/Main.java
+++ src/java/com/javarush/cryptoanalyser/Main.java
@@ -3,7 +3,10 @@ package com.javarush.cryptoanalyser;
 public class Main {
     public static void main(String[] args) {
         CaesarEncoder caesarEncoder = new CaesarEncoder();
-        caesarEncoder.decryptWithKey(5,"C:\\Users\\jinxt\\OneDrive\\Desktop\\TestFile2.txt","C:\\Users\\jinxt\\OneDrive\\Desktop\\TestFile.txt");
+        caesarEncoder.encryptWithKey(5,"C:\\Users\\jinxt\\OneDrive\\Desktop\\TestFile.txt",
+                "C:\\Users\\jinxt\\OneDrive\\Desktop\\TestFile2.txt");
+        caesarEncoder.decryptBruteForce("C:\\Users\\jinxt\\OneDrive\\Desktop\\TestFile2.txt",
+                "C:\\Users\\jinxt\\OneDrive\\Desktop\\TestFile3.txt");
     }
 
 }
